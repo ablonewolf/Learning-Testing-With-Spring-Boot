ec3779d7c923ad9951e634de34fd77ae9db33bcf
Arka Bhuiyan <os-arka.bhuiyan@rakuten.com>
2023-04-07 10:17:59 +0600

    Implemented assertNull and assertNotNull.

--- JUnit-Demo/src/test/java/com/arka99/JUnitDemo/DemoUtilsTest.java
+++ JUnit-Demo/src/test/java/com/arka99/JUnitDemo/DemoUtilsTest.java
@@ -2,8 +2,7 @@ package com.arka99.JUnitDemo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DemoUtilsTest {
     @Test
@@ -18,4 +17,12 @@ class DemoUtilsTest {
         assertNotEquals(9, demoUtils.add(4, 4), "4 + 4 should not be 9.");
     }
 
+    @Test
+    void testNullOrNotNull() {
+        DemoUtils demoUtils = new DemoUtils();
+        String string1 = null;
+        String string2 = "java";
+        assertNull(demoUtils.checkNull(string1), "This object should be null.");
+        assertNotNull(demoUtils.checkNull(string2),"This should not be null.");
+    }
 }
